f93b85a3bb92735d83ca3dde108ed5e9f2db42cc
ndirpaya <ndirpaya@gmail.com>
2016-12-13 22:15:25 +0100

    Added Admin Migration & model for multiauthentication

--- database/migrations/2016_12_06_200342_create_admins_table.php
+++ database/migrations/2016_12_06_200342_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->integer('role_id')->unsigned();
+            $table->integer('status_id')->unisigned();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();;
         });
     }
 
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('admins');
     }
 }
